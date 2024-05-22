7aba726e293b07dfc0e13c9d019c38997001c7e5
oxygensend <szymon19314@wp.pl>
2024-05-22 14:01:39 +0200

    add email index

--- src/main/java/com/oxygensend/auth/infrastructure/mongo/UserMongo.java
+++ src/main/java/com/oxygensend/auth/infrastructure/mongo/UserMongo.java
@@ -5,11 +5,14 @@ import java.util.UUID;
 import lombok.Builder;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.stereotype.Indexed;
 
 @Profile("mongo")
 @Builder
 @Document("users")
+@CompoundIndex(name = "email", def = "{'email': 1}")
 record UserMongo(@Id UUID id,
                  String email,
                  String username,
