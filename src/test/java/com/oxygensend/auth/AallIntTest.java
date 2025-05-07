8e9ea7580aa36236dfa07f0df33dfd5f240d407c
oxygensend <szymon19314@wp.pl>
2025-05-07 13:11:27 +0200

    chore: enhance JaCoCo configuration and skip tests in Maven build

---
+++ src/test/java/com/oxygensend/auth/AallIntTest.java
@@ -0,0 +1,16 @@
+package com.oxygensend.auth;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AallIntTest {
+
+
+    @Test
+    void test() {
+        // This is a placeholder test to ensure the class is recognized as a test class.
+        // You can add actual integration tests here.
+        assertTrue(true, "This is a placeholder test.");
+    }
+}
